342102f67a3ade0bc6b3664e1ba8334fe7a6f964
ramboanj <j.ramboaniaina@yahoo.fr>
2020-12-23 22:01:44 +0100

    successuf building without super

--- pythonforandroid/recipes/dlib/__init__.py
+++ pythonforandroid/recipes/dlib/__init__.py
@@ -14,7 +14,7 @@ class DlibRecipe(CppCompiledComponentsPythonRecipe):
     site_packages_name = 'dlib'
     version = '19.17'
     url = 'http://dlib.net/files/dlib-{version}.zip'
-    depends = ['python3','numpy','boost','cmake']
+    depends = ['python3','numpy','scipy','opencv']
 
 #    built_libraries = {"*.so" : "build/lib.dlib"}
 
@@ -45,6 +45,7 @@ class DlibRecipe(CppCompiledComponentsPythonRecipe):
         info(str(self.get_recipe_env(arch)));
         info(str(self.ctx.python_recipe))
 
+        
     def build_arch(self, arch):
         build_dir = join(self.get_build_dir(arch.arch), 'build')
         shprint(sh.mkdir, '-p', build_dir)
@@ -65,87 +66,81 @@ class DlibRecipe(CppCompiledComponentsPythonRecipe):
             python_include_opencv = join(python_site_packages,
                                              'opencv', 'core', 'include')
                 #Create a libçdir to put building 
-            lib_dir = join(build_dir,"lib.dlib")
+            lib_dir = join(build_dir,"lib")
 
             info("Create "+lib_dir)                                                   
             shprint(sh.mkdir, '-p', lib_dir)
-            
-            
-            
-            newArgs=[                                                                    
- #                    "-D P4A=ON" ,                                                        
-#                     "-D ANDROID_ABI={}".format(arch.arch),                               
-#                     "-D ANDROID_STANDALONE_TOOLCHAIN={}".format(self.ctx.ndk_dir),       
-#                     "-D ANDROID_NATIVE_API_LEVEL={}".format(self.ctx.ndk_api),           
-#                     "-D ANDROID_EXECUTABLE={}/tools/android".format(env["ANDROID_SDK"]), 
-#                     "-D CMAKE_TOOLCHAIN_FILE={}".format(                                 
-#                         join(self.ctx.ndk_dir, "build", "cmake",                         
-#                              "android.toolchain.cmake")),                                
- #                    # Make the linkage with our python library, otherwise we             
- #                    # will get dlopen error when trying to import dlib"s module.         
-#                      "-D CMAKE_SHARED_LINKER_FLAGS=-L{path} -lpython{version}".format(   
-#                          path=python_link_root,                                          
-#                          version=python_link_version),                                   
-                                                                                         
-#                     "-D BUILD_WITH_STANDALONE_TOOLCHAIN=ON",                             
- #                    # Force to build as shared libraries the dlib"s dependant            
- #                    # libs or we will not be able to link with our python                
-                      "-D BUILD_SHARED_LIBS=ON",                                           
-                      "-D BUILD_STATIC_LIBS=OFF",                                          
- #                    "-D LLVM_LIBDIR_SUFFIX=64",
- #                    # Disable some dlib"s features                                       
- #                    "-D BUILD_dlib_java=OFF",                                            
- #                    "-D BUILD_dlib_java_bindings_generator=OFF",                         
- #                    # "-D BUILD_dlib_highgui=OFF",                                       
- #                    # "-D BUILD_dlib_imgproc=OFF",                                       
- #                    # "-D BUILD_dlib_flann=OFF",                                         
-                      "-D BUILD_TESTS=OFF",                                                
-                      "-D BUILD_PERF_TESTS=OFF",                                           
-                      "-D ENABLE_TESTING=OFF",                                             
- #                    "-D BUILD_EXAMPLES=OFF",                                             
- #                    "-D BUILD_ANDROID_EXAMPLES=OFF",                                     
- #                    "-D BUILD_ANDROID_EXAMPLES=OFF",                                     
- #                    # Create sub-directory into build to ouput it                        
-                      "-D CMAKE_LIBRARY_OUTPUT_DIRECTORY={output}".format(output=lib_dir),
-            #         "-D CMAKE_LIBRARY_OUTPUT_NAME=libdlib",
- #                    # Force to only build our version of python                          
- #                    "-D BUILD_DLIB_PYTHON=ON".format(major=python_major),                
- #                    "-D BUILD_DLIB_PYTHON=OFF".format(                                   
- #                        major="2" if python_major == "3" else "3"),                      
-                                                                                         
- #                    # Force to install the `dlib.so` library directly into               
- #                    # python"s site packages (otherwise the dlib"s loader fails          
- #                    # on finding the dlib.so library)                                    
- #                    "-D DLIB_SKIP_PYTHON_LOADER=OFF",                                    
-                     "-D DLIB_PYTHON_INSTALL_PATH={site_packages}".format(                
-                          site_packages=python_site_packages),                            
-                                                                                         
-                    # Define python"s paths for: exe, lib, includes, numpy...            
-                    "-D PYTHON_DEFAULT_EXECUTABLE={}".format(self.ctx.hostpython),       
-                    "-D PYTHON_EXECUTABLE={host_python}".format(                         
-                         host_python=self.ctx.hostpython),                               
-                    "-D PYTHON_INCLUDE_DIRS={include_path}".format(                      
-                         include_path=python_include_root),                              
-                    "-D PYTHON_LIBRARIES={python_lib}".format(                             
-                         python_lib=python_library),                                     
-                    "-D PYTHON_NUMPY_INCLUDE_DIRS={numpy_include}".format(               
-                         numpy_include=python_include_numpy),                            
-                    "-D PYTHON_PACKAGES_PATH={site_packages}".format(                    
-                         site_packages=python_site_packages)]                            
+            info("Code = cmake {python} -B {output}".format(output=lib_dir,python=join(self.get_build_dir(arch.arch), 'tools/python')))
+            shprint(sh.cmake,                        
+                   # execute CMake in Tools/Python to build
 
+                    '{python}'.format(python=join(self.get_build_dir(arch.arch), 'tools/python')),
+                    #install on tmp
+                    '-B{output}'.format(output=lib_dir),
+                    
+                    '-DP4A=ON' ,                                                                   
+                    '-DANDROID_ABI={}'.format(arch.arch),                                          
+                    '-DANDROID_STANDALONE_TOOLCHAIN={}'.format(self.ctx.ndk_dir),                  
+                    '-DANDROID_NATIVE_API_LEVEL={}'.format(self.ctx.ndk_api),                      
+                    '-DANDROID_EXECUTABLE={}/tools/android'.format(env['ANDROID_SDK']),            
+                    '-DCMAKE_TOOLCHAIN_FILE={}'.format(                                            
+                        join(self.ctx.ndk_dir, 'build', 'cmake',                                   
+                             'android.toolchain.cmake')),                                          
+                    # Make the linkage with our python library, otherwise we                       
+                    # will get dlopen error when trying to import dlib's module.                   
+                    '-DCMAKE_SHARED_LINKER_FLAGS=-L{path} -lpython{version}'.format(               
+                        path=python_link_root,                                                     
+                        version=python_link_version),                                              
+                                                                                                   
+                    '-DBUILD_WITH_STANDALONE_TOOLCHAIN=ON',                                        
+                    # Force to build as shared libraries the dlib's dependant                      
+                    # libs or we will not be able to link with our python                          
+                    '-DBUILD_SHARED_LIBS=ON',                                                      
+                    '-DBUILD_STATIC_LIBS=OFF',                                                     
+                                                                                                   
+                    # Disable some dlib's features                                                 
+                    '-DBUILD_dlib_java=OFF',                                                       
+                    '-DBUILD_dlib_java_bindings_generator=OFF',                                    
+                    # '-DBUILD_dlib_highgui=OFF',                                                  
+                    # '-DBUILD_dlib_imgproc=OFF',                                                  
+                    # '-DBUILD_dlib_flann=OFF',                                                    
+                    '-DBUILD_TESTS=OFF',                                                           
+                    '-DBUILD_PERF_TESTS=OFF',                                                      
+                    '-DENABLE_TESTING=OFF',                                                        
+                    '-DBUILD_EXAMPLES=OFF',                                                        
+                    '-DBUILD_ANDROID_EXAMPLES=OFF',                                                
+                    '-DBUILD_ANDROID_EXAMPLES=OFF',                                                
+                    # Create sub-directory into build to ouput it                                  
+                    '-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={output}'.format(output=lib_dir),            
+                    # Force to only build our version of python                                    
+                    '-DBUILD_DLIB_PYTHON{major}=ON'.format(major=python_major),                    
+                    '-DBUILD_DLIB_PYTHON{major}=OFF'.format(                                       
+                        major='2' if python_major == '3' else '3'),                                
+                                                                                                   
+                    # Force to install the `dlib.so` library directly into                         
+                    # python's site packages (otherwise the dlib's loader fails                    
+                    # on finding the dlib.so library)                                              
+                    '-DDLIB_SKIP_PYTHON_LOADER=OFF',                                               
+                    '-DDLIB_PYTHON{major}_INSTALL_PATH={site_packages}'.format(                    
+                        major=python_major, site_packages=python_site_packages),                   
+                                                                                                   
+                    # Define python's paths for: exe, lib, includes, numpy...                      
+                    '-DPYTHON_DEFAULT_EXECUTABLE={}'.format(self.ctx.hostpython),                  
+                    '-DPYTHON{major}_EXECUTABLE={host_python}'.format(                             
+                        major=python_major, host_python=self.ctx.hostpython),                      
+                    '-DPYTHON{major}_INCLUDE_PATH={include_path}'.format(                          
+                        major=python_major, include_path=python_include_root),                     
+                    '-DPYTHON{major}_LIBRARIES={python_lib}'.format(                               
+                        major=python_major, python_lib=python_library),                            
+                    '-DPYTHON{major}_NUMPY_INCLUDE_DIRS={numpy_include}'.format(                   
+                        major=python_major, numpy_include=python_include_numpy),                   
+                    '-DPYTHON{major}_PACKAGES_PATH={site_packages}'.format(                        
+                        major=python_major, site_packages=python_site_packages),                   
+                    self.get_build_dir(arch.arch),                                                 
+                    _env=env),                                                                     
+            #Install python bindings (dlib.so)
 
-#            buildTemp="--build-temp={output}".format(output=lib_dir)
-            
-#            self.setup_extra_args.append(buildTemp)                                                                                 
-            for arg in newArgs:                                                          
-                info(arg)                                                                
-                setArg=arg.split(" ")[1]                                                 
-                self.setup_extra_args.append("--set")                                    
-                self.setup_extra_args.append(setArg)                                     
-                                                                                         
-
-            super().build_arch(arch)
+            shprint(sh.cmake,'--build', '{output}'.format(output=lib_dir)  , '--config','RELEASE','--')                     
             sh.cp('-a', sh.glob('{}/*.so'.format(lib_dir))  ,self.ctx.get_libs_dir(arch.arch))     
-            self.setup_extra_args.clear()#After finished compiled ,remove all setup
             
 recipe = DlibRecipe()
