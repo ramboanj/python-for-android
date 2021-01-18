ec88be2f39179ae668d0770874d1574e50cb77b1
ramboanj <j.ramboaniaina@yahoo.fr>
2021-01-18 09:16:45 +0100

    add patch

--- pythonforandroid/recipes/dlib/__init__.py
+++ pythonforandroid/recipes/dlib/__init__.py
@@ -2,26 +2,28 @@ from pythonforandroid.recipe import CppCompiledComponentsPythonRecipe
 import sh
 from os.path import join
 from multiprocessing import cpu_count
-from pythonforandroid.logger import shprint
+from pythonforandroid.logger import (shprint,info)
 from pythonforandroid.util import current_directory
+from sys import platform,executable
 
 
 class DlibRecipe(CppCompiledComponentsPythonRecipe):
     site_packages_name = 'dlib'
-    version = '19.17'
-    url = 'http://dlib.net/files/dlib-{version}.zip'
-    depends = ['numpy','cmake']
+    version = '19.21'
+    #    url = 'http://dlib.net/files/dlib-{version}.zip'
+    url = "https://github.com/davisking/dlib/archive/v{version}.zip"
+    depends = ['numpy','cmake','pybind11']
     call_hostpython_via_targetpython = False
-
+    built_libraries = {"libdlib.so" : "build/libdlib"}
     def get_lib_dir(self, arch):
-        return join(self.get_build_dir(arch.arch), 'build', 'lib', arch.arch)
+        return join(self.get_build_dir(arch.arch), 'build', 'libdlib', arch.arch)
 
     def get_recipe_env(self, arch):
         env = super(DlibRecipe, self).get_recipe_env(arch)
         env['ANDROID_NDK'] = self.ctx.ndk_dir
         env['ANDROID_SDK'] = self.ctx.sdk_dir
         return env
-
+                    
     def build_arch(self, arch):
         build_dir = join(self.get_build_dir(arch.arch), 'build')
         shprint(sh.mkdir, '-p', build_dir)
@@ -42,7 +44,7 @@ class DlibRecipe(CppCompiledComponentsPythonRecipe):
             python_include_opencv = join(python_site_packages,
                                         'opencv', 'core', 'include')
             #Create a libçdir to put building 
-            lib_dir = join(build_dir,"lib")
+            lib_dir = join(build_dir,"libdlib")
             shprint(sh.mkdir, '-p', lib_dir)            
             shprint(sh.cmake,
                     #execute CMake in Tools/Python to build
@@ -64,9 +66,9 @@ class DlibRecipe(CppCompiledComponentsPythonRecipe):
                     '-DBUILD_WITH_STANDALONE_TOOLCHAIN=ON',
                     # Force to build as shared libraries the dlib's dependant
                     # libs or we will not be able to link with our python
-                    '-DBUILD_SHARED_LIBS=OFF',
+                    '-DBUILD_SHARED_LIBS=ON',
                     '-DBUILD_STATIC_LIBS=ON',
-
+                    '-B{output}'.format(output=lib_dir),
                     # Disable some dlib's features
                     '-DBUILD_dlib_java=OFF',
                     '-DBUILD_dlib_java_bindings_generator=OFF',
@@ -85,7 +87,7 @@ class DlibRecipe(CppCompiledComponentsPythonRecipe):
                     '-DBUILD_DLIB_PYTHON{major}=ON'.format(major=python_major),
                     '-DBUILD_DLIB_PYTHON{major}=OFF'.format(
                         major='2' if python_major == '3' else '3'),
-
+                    '-DCMAKE_BUILD_TYPE=RELEASE',
                     # Force to install the `dlib.so` library directly into
                     # python's site packages (otherwise the dlib's loader fails
                     # on finding the dlib.so library)
@@ -107,10 +109,8 @@ class DlibRecipe(CppCompiledComponentsPythonRecipe):
                         major=python_major, site_packages=python_site_packages),
                     self.get_build_dir(arch.arch),
                     _env=env),
-
-            # Install python bindings (dlib.so)
-#            shprint(sh.cmake, '-DCOMPONENT=python', '-P', './')
-            # Copy third party shared libs that we need in our final apk
-#            sh.cp('-a', sh.glob('./lib/{}/lib*.so'.format(arch.arch)),self.ctx.get_libs_dir(arch.arch))
+            with current_directory(lib_dir):
+                # Install python bindings (dlib.so)
+                shprint(sh.make)
 
 recipe = DlibRecipe()
